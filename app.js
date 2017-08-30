f2f1f050191e19f3c5f281a27defdb062dbf6a28
Shannon <reidy.pf@gmail.com>
2017-08-30 11:39:23 -0500

    added gui boilerplate and homepage

--- app.js
+++ app.js
@@ -1,24 +1,72 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const sequelize = require('sequelize');
 const models = require('./models');
 
-function createUser(){
-  const user = models.User.build({
-    name: 'Shannon Reidy',
-    email: 'shannonf.reidy@gmail.com',
-    bio: 'sorceress'
-  });
+let app = express();
 
-user.save()
-.then(function(newUser){
-  console.log(newUser.dataValues);
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.get('/', function(req, res) {
+  res.render('index');
 })
 
-}
+app.listen(3000, function() {
+  console.log('successfully started Express Application');
+})
 
-function listUsers(){
-  models.User.findAll().then(function(users){
-    users.forEach(function(user){
-      console.log(user.dataValues);
-    })
-  })
-}
-listUsers();
+process.on('SIGINT', function() {
+  console.log("\nshutting down");
+  const index = require('./models/index')
+  index.sequelize.close()
+
+ // give it a second
+  setTimeout(function() {
+    console.log('process exit');
+    process.exit(0);
+  }, 1000)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createUser(){
+//   const user = models.User.build({
+//     name: 'Shannon Reidy',
+//     email: 'shannonf.reidy@gmail.com',
+//     bio: 'sorceress'
+//   });
+//
+// user.save()
+// .then(function(newUser){
+//   console.log(newUser.dataValues);
+// })
+//
+// }
+//
+// function listUsers(){
+//   models.User.findAll().then(function(users){
+//     users.forEach(function(user){
+//       console.log(user.dataValues);
+//     })
+//   })
+// }
+// listUsers();
