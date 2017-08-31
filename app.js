9f4363b05a50027c15259b4ae7fd609c7593b1ae
Shannon <reidy.pf@gmail.com>
2017-08-31 10:55:39 -0500

    CRUD

--- app.js
+++ app.js
@@ -18,11 +18,75 @@ app.use(bodyParser.urlencoded({
 app.get('/', function(req, res) {
   res.render('index');
 })
-app.get('/users', function(req,res){
+app.get('/users', function(req, res) {
   models.User.findAll()
-  .then(function(userslist){
-     res.render('usersgui', {userskey:userslist});
-   })
+    .then(function(userslist) {
+      res.render('usersgui', {
+        userskey: userslist
+      });
+    })
+})
+app.get('/adduser', function(req, res) {
+  res.render('adduser');
+})
+app.post('/adduser', function(req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const bio = req.body.bio;
+  const user = models.User.build({
+    name: name,
+    email: email,
+    bio: bio
+  });
+
+  user.save()
+    .then(function() {
+      res.redirect('/users')
+    })
+
+})
+app.post('/delete/:id', function(req, res) {
+  let id = req.params.id;
+  models.User.destroy({
+      where: {
+        id: id
+      }
+    })
+    .then(function() {
+      res.redirect('/users');
+    })
+})
+app.get('/update/:id', function(req, res) {
+  let id = req.params.id;
+  models.User.findOne({
+      where: {
+        id: id
+      }
+    })
+    .then(function(user) {
+      res.render('updateuser', {
+        userskey: user
+      })
+    })
+})
+app.post('/update/:id', function(req, res) {
+  let id = req.params.id
+  const upname = req.body.name;
+  const upemail = req.body.email;
+  const upbio = req.body.bio;
+  const user = models.User.update({
+    name: upname,
+    email: upemail,
+    bio: upbio
+  }, {
+    where: {
+      id: id
+    }
+  })
+    .then(function() {
+      res.redirect('/users')
+    })
+
 })
 
 app.listen(3000, function() {
@@ -34,17 +98,12 @@ process.on('SIGINT', function() {
   const index = require('./models/index')
   index.sequelize.close()
 
- // give it a second
+  // give it a second
   setTimeout(function() {
     console.log('process exit');
     process.exit(0);
   }, 1000)
 });
-
-
-
-
-
 
 
 
